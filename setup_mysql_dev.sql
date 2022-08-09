f4056b58bc6ccadaed3b7208554e4bb100d166d9
louay66 <louaylouay20@gmail.com>
2022-08-09 18:07:04 +0100

    setup server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+--CREATE DATABASE name hbnb_dev_db
+--CREATE USER named hbnb_dev and password hbnb_dev_pwd
+--GRANT ALL PRIVILEGES
+--GRANT SELECT ON performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
