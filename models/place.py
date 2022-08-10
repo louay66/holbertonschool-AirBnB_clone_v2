e8bf1a54b09bcbcf73eba9f5f5b3c1fac2e32db1
louay66 <louaylouay20@gmail.com>
2022-08-10 23:15:55 +0100

    is done

--- models/place.py
+++ models/place.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+class Place(BaseModel, Base):
+    """represont a User from database
+     __tablename__: the name of sql table
+    city_id (sqlalchemy.String): id of city
+    user_id (sqlalchemy.String): id of  user
+    name (sqlalchemy.String): The name of Place
+    description (sqlalchemy.String): The description of user
+    number_rooms (sqlalchemy.Integer): The number_rooms in place
+    number_bathrooms (sqlalchemy.Integer): The number_bathrooms in place
+    max_guest (sqlalchemy.Integer: The max_guest in place
+    price_by_night (sqlalchemy.Integer): The price_by_night in place
+    latitude (sqlalchemy.Float): The latitude of place
+    ongitude (sqlalchemy.Float): The ongitude of place
+   """
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models.city import City
 from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -19,3 +19,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
