5fe2db94e853462b46d8b45dc0d676fe627fcb3a
louay66 <louaylouay20@gmail.com>
2022-08-10 22:55:08 +0100

    is done

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.city import City
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """represont a User from database
+     __tablename__: the name of sql table
+    email (sqlalchemy.String): The emial of user
+    password (sqlalchemy.String): The password of user
+    first_name (sqlalchemy.String): The first_name of user
+    last_name (sqlalchemy.String): The last_name of user
+   """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
