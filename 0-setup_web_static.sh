9c43f3673928b55d1cf283e6110332af3b4dde70
louay66 <louaylouay20@gmail.com>
2022-08-25 18:12:05 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 # config ngnix
-sudo apt-get -y update && upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+config="
+server {
+        listen 80 default_server;
+
+        listen [::]:80 default_server;
+
+
+        server_name _;
+
+        location / {
+
+        try_files \$uri \$uri/ =404;
+
+        }
+
+        location /hbnb_static {
+
+        alias /data/web_static/current;
+        index index.html;
+        try_files \$uri \$uri /hbnb_static/index.html;
+
+        }
+
+}
+"
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo chmod 666 /etc/nginx/sites-available/default
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 echo $'<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '37i\\tlocation /hbnb_static/ {\nalias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
-sudo service nginx stop
-sudo service nginx start
+sudo echo -e "$config" > /etc/nginx/sites-available/default
+sudo service nginx restart
